707fa7d629448c05e62bdf1618ed9b2d47855349
Olga <o.f.akhmetova@gmail.com>
2019-12-13 13:09:54 +0300

    Add possibility to convert to epub file

--- README.md
+++ README.md
@@ -9,14 +9,19 @@ Usage
 
 Only required argument is url source string, others are optional: 
 
-    -h, --help     show this help message and exit
-    -v, --version  show version and exit
-    --json         show content in json format
-    --verbose      show verbose status messages
-    --limit LIMIT  news limit
-    --date DATE    date for cached news in format YYYYMMDD
-
-If ``limit`` is not provided, tool outputs all content.
+    positional arguments:
+      source                rss source
+    
+    optional arguments:
+      -h, --help            show this help message and exit
+      -v, --version         show version and exit
+      --json                show content in json format
+      --verbose             show verbose status messages
+      --limit LIMIT         news limit
+      --date DATE           date for cached news in format YYYYMMDD
+      --to-epub             save data in epub file
+      --output-path OUTPUT_PATH
+                            path to the new file for saving
 
 If ``json`` argument is provided, tool returns data as json object with following structure:
 
@@ -42,8 +47,14 @@ If ``json`` argument is provided, tool returns data as json object with followin
         ]
     }
 
+If ``limit`` is not provided, tool outputs all content.
+
 If ```date``` argument is provided, news for this day are seeked in cache file. If there is no news, error is returned.
 All other arguments work with this ```date``` argument as expected.
+
+If ```to-epub```  argument is provided, news will be converted and saved in .epub file. Optional argument
+```output-path``` allows to specify path for file. If it is invalid or is not specified, file wiil be saved in current directory. If ```to-epub```
+argument is missing, ```output-path``` will be ignored. If ```json``` argument is present, ```to-epub``` will be ignored.
 
 To run tool as command line utility:
 
--- rss_reader.py
+++ rss_reader.py
@@ -1,17 +1,23 @@
 """Utility to print RSS in console in readable format"""
 # !/usr/bin/env python
 import argparse
+from bs4 import BeautifulSoup
 import datetime
+from ebooklib import epub
 import feedparser
 from feedparser.util import FeedParserDict
 import html2text
+import io
 import json
 from typing import List, Dict, Optional
 from dateutil.parser import parse as parse_date, ParserError
 import logging
 import os
+from pathlib import Path
 import pickle
+from PIL import Image
 import sys
+import urllib.request
 
 VERSION = "0.3"
 CACHE_FILE = "cache"
@@ -94,6 +100,70 @@ def process_formatted_output(feed: FeedParserDict, limit: int) -> List[Optional[
     return result
 
 
+def process_images(images, book):
+    """Download images by url to local file and add to epub book"""
+    for img in images:
+        src = str(hash(img.get("src"))) + ".jpg"
+        urllib.request.urlretrieve(img.get("src"), src)
+        img["src"] = src
+        try:
+            image = Image.open(src)
+            image_rgb = image.convert("RGB")
+            image_stream = io.BytesIO()
+            image_rgb.save(image_stream, 'jpeg')
+            image_content = image_stream.getvalue()
+            image_item = epub.EpubItem(uid=str(hash(src)), file_name=src, media_type='image/jpeg',
+                                       content=image_content)
+            book.add_item(image_item)
+        except Exception:
+            pass
+        try:
+            os.remove(src)
+        except Exception:
+            pass
+
+
+def create_title(title: str) -> str:
+    """Add title to news"""
+    return "<h3>" + title + "</h3>"
+
+
+def create_link(link: str) -> str:
+    """Add link to news on external resource"""
+    return "<a href='" + link + "'>Link</a>"
+
+
+def process_output_to_epub(feed: FeedParserDict, limit: int) -> epub.EpubBook:
+    """Form epub structure for epub file"""
+    logging.debug("Starting format html for epub file")
+    book = epub.EpubBook()
+    book.set_identifier('rss news')
+    book.set_title(feed.get("feed", {}).get("title"))
+    book.set_language('en')
+
+    book.spine = ['nav']
+    book.toc = []
+    for news in feed.get("entries")[:limit]:
+        chapter = epub.EpubHtml(title=news.get("title"), file_name=str(hash(news.get("title"))) + '.xhtml')
+        content = BeautifulSoup(create_title(news.get("title")) + news.get("summary") + create_link(news.get("link")),
+                                "lxml")
+        images = content.find_all('img')
+        process_images(images, book)
+        chapter.set_content(str(content))
+        book.add_item(chapter)
+        book.spine.append(chapter)
+        book.toc.append(chapter)
+
+    book.add_item(epub.EpubNcx())
+    book.add_item(epub.EpubNav())
+
+    style = 'BODY {color: white;}'
+    nav_css = epub.EpubItem(uid="style_nav", file_name="style/nav.css", media_type="text/css", content=style)
+    book.add_item(nav_css)
+    logging.debug("Structure for epub file created")
+    return book
+
+
 def parse_args(args):
     """Create argument parser, parse arguments and return them"""
     arg_parser = create_arg_parser("Get rss source and show it's content")
@@ -103,6 +173,8 @@ def parse_args(args):
     arg_parser.add_argument("--verbose", action="store_true", help="show verbose status messages", default=False)
     arg_parser.add_argument("--limit", action="store", help="news limit", default=-1)
     arg_parser.add_argument("--date", action="store", help="date for cached news in format YYYYMMDD")
+    arg_parser.add_argument("--to-epub", action="store_true", help="save data in epub file", default=False)
+    arg_parser.add_argument("--output-path", action="store", help="path to the new file for saving")
     return arg_parser.parse_args(args)
 
 
@@ -150,6 +222,17 @@ def get_feed_from_cache(source, date) -> Dict:
     return feed
 
 
+def check_path(path: str) -> bool:
+    """Check if passed dir exists and filename has right extension"""
+    return path and os.path.exists(os.path.dirname(path)) and os.path.basename(path).endswith(".epub")
+
+
+def get_default_path() -> str:
+    """Get home directory if path is not specified"""
+    print("Path is invalid or not specified. Creating news.epub file in home directory", str(Path.home()))
+    return os.path.join(str(Path.home()), "news.epub")
+
+
 def run(argv) -> None:
     """Main function. Parses arguments and prints result in desired form.
     Argument "argv" is added for testing"""
@@ -162,6 +245,8 @@ def run(argv) -> None:
         limit = int(arguments.limit)
     except Exception:
         raise error("argument --limit: expected number")
+    if arguments.output_path and not arguments.to_epub:
+        print("Argument --output-path will be ignored")
     logging.debug("Arguments parsed")
 
     logging.debug("Parsing data...")
@@ -186,11 +271,23 @@ def run(argv) -> None:
     logging.debug("Data parsed")
 
     if json_arg:
+        if arguments.to_epub:
+            print("Argument --to-epub will be ignored")
         result = process_json(feed, limit)
         print(json.dumps(result, indent=4))
     else:
-        result = process_formatted_output(feed, limit)
-        [print(chunk) for chunk in result if chunk]
+        if arguments.to_epub:
+            path = arguments.output_path if check_path(arguments.output_path) else get_default_path()
+            try:
+                epub_data = process_output_to_epub(feed, limit)
+                epub.write_epub(path, epub_data, {})
+            except Exception as e:
+                raise error(str(e))
+            else:
+                print("Epub file", path, "was successfully created")
+        else:
+            result = process_formatted_output(feed, limit)
+            [print(chunk) for chunk in result if chunk]
     logging.debug("Work is done")
 
 
